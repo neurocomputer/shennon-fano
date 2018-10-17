480a7aa3d4d8b08353514e22fdc24a558528af7b
Sergey <seach@inbox.ru>
2018-10-18 00:08:42 +0300

    change old script

--- main_example1.py
+++ main_example1.py
@@ -1,15 +1,22 @@
 import shennonfano as sf
 
+#сообщение для кодирования
 word = 'mississippi'
 
+#генерируем код с помощью модуля shennonfano
 code = sf.get_code(word)
 
+#выводим на экран код
 print(code)
 
+#кодируем сообщение
 coded_message = sf.code_message(word)
 
+#выводим на экран кодированное сообщение
 print(coded_message)
 
+#декодируем сообщение
 decoded_message = sf.decode_message(coded_message)
 
+#выводим на экран декодированное сообщение
 print(decoded_message)
--- main_example2.py
+++ main_example2.py
@@ -5,3 +5,11 @@ word = 'mississippi'
 get_code(word)
 
 print(code)
+
+coded_message = code_message(word)
+
+print(coded_message)
+
+decoded_message = decode_message(coded_message)
+
+print(decoded_message)
