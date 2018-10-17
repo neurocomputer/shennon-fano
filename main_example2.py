6c08ce5c803fee891eb6bbbc3559755c748c4b0c
Sergey Shchanikov <43891297+neurocomputer@users.noreply.github.com>
2018-10-18 00:27:56 +0300

    Update main_example2.py

--- main_example2.py
+++ main_example2.py
@@ -1,15 +1,22 @@
 from shennonfano import *
 
+#сообщение для кодирования
 word = 'mississippi'
 
+#генерируем код с помощью модуля shennonfano
 get_code(word)
 
+#выводим на экран код
 print(code)
 
+#кодируем сообщение
 coded_message = code_message(word)
 
+#выводим на экран кодированное сообщение
 print(coded_message)
 
+#декодируем сообщение
 decoded_message = decode_message(coded_message)
 
+#выводим на экран декодированное сообщение
 print(decoded_message)
