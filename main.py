b6b2901efd430574320fe88389377f23a78040fd
Sergey <seach@inbox.ru>
2018-10-18 00:50:40 +0300

    change main.py

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import shennonfano as sf
 
+#выбираем текст для кодирования
 f = open('sample.txt', 'r')
-
 message = f.read()
-print(len(message))
 
+#генерируем код с помощью модуля shennonfano
 code = sf.get_code(message)
 
+#выводим на экран код
 print(code)
 
+#кодируем сообщение
 coded_message = sf.code_message(message)
 
+#выводим на экран кодированное сообщение
 print(coded_message)
 
+#выводим на экран размер сообщения в байтах
 print(len(coded_message)/8)
 
+#декодируем сообщение
 decoded_message = sf.decode_message(coded_message)
 
+#выводим на экран декодированное сообщение
 print(decoded_message)
 
+#сравниваем длину до кодирования и после декодирования
 print('Длина исходная:',len(message),'Длина полученная:',len(decoded_message))
